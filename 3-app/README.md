01565bf3b39186ec1148acf6a85e8c956811f661
Zach Cristol <zachcristol@gmail.com>
2019-02-20 19:48:56 -0600

    another commit

--- 3-app/README.md
+++ 3-app/README.md
@@ -1,6 +1,5 @@
-Basic react app by rendering JSX in ReactDOM.render   
-changed
-again
+Creating and rendering react components with and without props passed to them.
+
 ## Usage
 
 ### `npm start`
@@ -8,4 +7,6 @@ again
 Runs the app in the development mode.  
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.  
 
-The page will reload if you make edits.
+The page will reload if you make edits.  
+
+All necessary code is in index.js
--- 3-app/src/index.js
+++ 3-app/src/index.js
@@ -3,20 +3,40 @@ import React from 'react'
 // gives us the ability to render react to the dom
 import ReactDOM from 'react-dom'
 
-// Specified style
-var style = {
-    backgroundColor: 'Orange',
-    color: 'white',
-    fontFamily: 'Arial'
+
+
+// Making a component
+class Message extends React.Component {
+    // All components have a render method
+    // What we want to render to the dom
+    render() {
+        return (
+            <div>
+                <h1>Hello errrbody</h1>
+            </div>
+        )
+    }
+}
+
+// Making a component that uses props
+class PropsMessage extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1 style={ {color: this.props.color} }>
+                    {this.props.msg}
+                </h1>
+                <p>Ill check back in {this.props.time} minutes</p>
+            </div>
+        )
+    }
 }
 
 // We can render JSX (similar to html)
 ReactDOM.render(
-    // Render JSX
-    <div style={style}>
-        <h1>Hello there</h1>
-        <p>This is a desciription</p> 
-    </div>
+    //Render the react component
+    // <Message/>
+    <PropsMessage color={"blue"} time={50} msg="hello therre thingo"/>
     , 
     document.getElementById('root') // Where do you want to render (public/index.html)
 )
